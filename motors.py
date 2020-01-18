c50c0e6ab643993958f9f0a63905ed538a7ac67a
mcisek <cisek.dev@gmail.com>
2020-01-18 16:58:15 +0100

    ADD create implementation for motors class

--- motors.py
+++ motors.py
@@ -1,65 +1,79 @@
 import wiringpi
 import cv2
 
-A1 = 5
-B1 = 6
-A2 = 20
-B2 = 21
-PWM1 = 13
-PWM2 = 19
+class Motors:
+    A1 = 5
+    B1 = 6
+    A2 = 20
+    B2 = 21
+    PWM1 = 13
+    PWM2 = 19
+
+    def setup_motors(self):
+        wiringpi.pinMode(self.A1, 1)
+        wiringpi.pinMode(self.A2, 1)
+        wiringpi.pinMode(self.B1, 1)
+        wiringpi.pinMode(self.B2, 1)
+        wiringpi.softPwmCreate(self.PWM1, 1, 100)
+        wiringpi.softPwmCreate(self.PWM2, 1, 100)
+
+    def move_right_motor(self, forward, pwm):
+        if forward:
+            wiringpi.digitalWrite(self.A1, 1)
+            wiringpi.digitalWrite(self.B1, 0)
+        else:
+            wiringpi.digitalWrite(self.A1, 0)
+            wiringpi.digitalWrite(self.B1, 1)
+        if 0 == pwm:
+            wiringpi.digitalWrite(self.A1, 1)
+            wiringpi.digitalWrite(self.B1, 1)
+        else:
+            wiringpi.softPwmWrite(self.PWM1, pwm)
+
+    def move_left_motor(self, forward, pwm):
+        if forward:
+            wiringpi.digitalWrite(self.A2, 0)
+            wiringpi.digitalWrite(self.B2, 1)
+        else:
+            wiringpi.digitalWrite(self.A2, 1)
+            wiringpi.digitalWrite(self.B2, 0)
+        if 0 == pwm:
+            wiringpi.digitalWrite(self.A2, 1)
+            wiringpi.digitalWrite(self.B2, 1)
+        else:
+            wiringpi.softPwmWrite(self.PWM2, pwm)
 
 if (__name__ == '__main__'):
     wiringpi.wiringPiSetupGpio()
     img = cv2.imread('robot.jpg')
     cv2.imshow('Robot', img)
 
-    wiringpi.pinMode(A1, 1)
-    wiringpi.pinMode(B1, 1)
-    wiringpi.pinMode(A2, 1)
-    wiringpi.pinMode(B2, 1)
-    wiringpi.softPwmCreate(PWM1, 1, 100)
-    wiringpi.softPwmCreate(PWM2, 1, 100)
+    motors = Motors()
+    motors.setup_motors()
     pwm = 10
-    wiringpi.softPwmWrite(PWM1, pwm)
-    wiringpi.softPwmWrite(PWM2, pwm)
 
     while(True):
         key = cv2.waitKey(1) & 0xFF
 
-
-
         if key == 27: #exit
-            print("exit")
-            wiringpi.digitalWrite(A1, 1)
-            wiringpi.digitalWrite(B1, 1)
-            wiringpi.digitalWrite(A2, 1)
-            wiringpi.digitalWrite(B2, 1)
+            motors.move_right_motor(True, 0)
+            motors.move_left_motor(True, 0)
             quit()
         elif key == 119: #up
-            wiringpi.digitalWrite(A1, 1)
-            wiringpi.digitalWrite(B1, 0)
-            wiringpi.digitalWrite(A2, 0)
-            wiringpi.digitalWrite(B2, 1)
+            motors.move_right_motor(True, pwm)
+            motors.move_left_motor(True, pwm)
         elif key == 115: #down
-            wiringpi.digitalWrite(A1, 1)
-            wiringpi.digitalWrite(B1, 1)
-            wiringpi.digitalWrite(A2, 1)
-            wiringpi.digitalWrite(B2, 1)
+            motors.move_right_motor(True, 0)
+            motors.move_left_motor(True, 0)
         elif key == 97: #left
-            wiringpi.digitalWrite(A1, 1)
-            wiringpi.digitalWrite(B1, 0)
-            wiringpi.digitalWrite(A2, 1)
-            wiringpi.digitalWrite(B2, 1)
+            motors.move_right_motor(True, pwm)
+            motors.move_left_motor(True, 0)
         elif key == 100: #right
-            wiringpi.digitalWrite(A1, 1)
-            wiringpi.digitalWrite(B1, 1)
-            wiringpi.digitalWrite(A2, 0)
-            wiringpi.digitalWrite(B2, 1)
+            motors.move_right_motor(True, 0)
+            motors.move_left_motor(True, pwm)
         elif key == 45: #plus
             pwm = pwm + 1
-            wiringpi.softPwmWrite(PWM1, pwm)
-            wiringpi.softPwmWrite(PWM2, pwm)
+            print(pwm)
         elif key == 48: #minus
             pwm = pwm - 1
-            wiringpi.softPwmWrite(PWM1, pwm)
-            wiringpi.softPwmWrite(PWM2, pwm)
+            print(pwm)
